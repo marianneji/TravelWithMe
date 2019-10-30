0073a1eba6a6781ab2f1682b079ccbdc18168004
marianneji <marianneji@gmail.com>
2019-10-30 16:28:05 +0100

    empty ApiKey

---
+++ Travel/ApiKey.swift
@@ -0,0 +1,13 @@
+//
+//  ApiKey.swift
+//  Travel
+//
+//  Created by Graphic Influence on 30/10/2019.
+//  Copyright © 2019 marianne massé. All rights reserved.
+//
+
+import Foundation
+
+struct Apikey {
+
+}
