ae74a149f548361a2d7c146b87c26735455db78d
marianneji <marianneji@gmail.com>
2019-11-12 14:28:54 +0100

    Add button on keyBoard, add extension UIPIckerViewDelegate, add a selecteRow method

--- Travel/Controllers/TranslateViewController.swift
+++ Travel/Controllers/TranslateViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class TranslateViewController: UIViewController, UIPickerViewDelegate, UITextViewDelegate, UIPickerViewDataSource {
+class TranslateViewController: UIViewController,  UITextViewDelegate, UIPickerViewDataSource {
 
 
 
+    @IBOutlet weak var detectedLanguageLabel: UILabel!
     @IBOutlet weak var inputTextView: UITextView!
     @IBOutlet weak var outputTextView: UITextView!
     @IBOutlet weak var languagePickerView: UIPickerView!
@@ -25,51 +26,45 @@ class TranslateViewController: UIViewController, UIPickerViewDelegate, UITextVie
         outputTextView.delegate = self
         languagePickerView.dataSource = self
         languagePickerView.delegate = self
+        addDoneButtonOnKeyboard()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         fetchSupportedLanguage()
 
-//        // Observe for keyboard notifications.
-//        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardWillAppear(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-//        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        outputTextView.text = "Press translate to see your translation here"
     }
-//    deinit {
-//        NotificationCenter.default.removeObserver(self)
-//    }
+
     func textViewDidBeginEditing(_ textView: UITextView) {
         inputTextView.text = ""
+        outputTextView.text = ""
     }
-//    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-//        <#code#>
-//    }
+
+    fileprivate func detectLanguage() {
+        TranslateManager.shared.detectLanguage(forText: inputTextView.text) { (language) in
+            if let language = language {
+                self.detectedLanguageLabel.text? = "Dectected language : \(language)"
+            }
+        }
+    }
+
     func textViewDidEndEditing(_ textView: UITextView) {
         if inputTextView.text != "" {
-            textView.resignFirstResponder()
+
+//            detectLanguage()
+            inputTextView.resignFirstResponder()
         } else {
             inputTextView.text = "Enter your word here"
         }
     }
 
-
-
-
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return TranslateManager.shared.supportedLanguage.count
-    }
-
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return TranslateManager.shared.supportedLanguage[row].name
-    }
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        TranslateManager.shared.targetLanguageCode = TranslateManager.shared.supportedLanguage[row].code
-    }
-
     @IBAction func translatePressed(_ sender: UIButton) {
-        // implementer detectlanguage et donner une valeur à sourceCode
+
         if inputTextView.text != "" {
+
             TranslateManager.shared.textToTranslate = inputTextView.text
+            inputTextView.resignFirstResponder()
         }
         TranslateManager.shared.translate { (translation) in
             if let translation = translation {
@@ -89,28 +84,51 @@ class TranslateViewController: UIViewController, UIPickerViewDelegate, UITextVie
                 DispatchQueue.main.async {
                     [unowned self] in
                     self.languagePickerView.reloadAllComponents()
+                    if let englishRow = TranslateManager.shared.languageCode.firstIndex(of: "en") {
+                    self.languagePickerView.selectRow(englishRow, inComponent: 0, animated: true)
+                        TranslateManager.shared.targetLanguageCode = "en"
+                    }
                 }
             } else {
                 print("il y a une erreur, trouve la")
             }
         }
     }
-//    @objc func handleKeyboardWillAppear(notification: Notification) {
-//        guard let userInfo = notification.userInfo else { return }
-//        if let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
-//            let keyboardHeight = keyboardSize.size.height
-//            bottomInputTextViewConstraint.constant = 20.0
-//            bottomInputTextViewConstraint.constant += keyboardHeight
-//        }
-//    }
-//
-//
-//    @objc func handleKeyboardWillHide(notification: Notification) {
-//        guard let userInfo = notification.userInfo else { return }
-//        if let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
-//            let keyboardHeight = keyboardSize.size.height
-//            bottomInputTextViewConstraint.constant -= keyboardHeight
-//        }
-//    }
+
+    func addDoneButtonOnKeyboard(){
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        inputTextView.inputAccessoryView = doneToolbar
+    }
+    @objc func doneButtonAction(){
+        inputTextView.resignFirstResponder()
+    }
+}
+//MARK: - UIPICKERVIEWDELEGATE
+extension TranslateViewController: UIPickerViewDelegate {
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+           return TranslateManager.shared.supportedLanguage.count
+       }
+
+       func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+           return TranslateManager.shared.supportedLanguage[row].name
+       }
+
+       func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+           TranslateManager.shared.targetLanguageCode = TranslateManager.shared.supportedLanguage[row].code
+           inputTextView.resignFirstResponder()
+           print("\(TranslateManager.shared.supportedLanguage[row].code) = \(row)")
+       }
+
 
 }
