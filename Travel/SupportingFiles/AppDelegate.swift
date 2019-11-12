593110c60c1214b16ec4a5fbf05adb3a6ee9084c
marianneji <marianneji@gmail.com>
2019-11-12 15:06:21 +0100

    add UITextViewWorkaround to app delegate to debug TextView

--- Travel/SupportingFiles/AppDelegate.swift
+++ Travel/SupportingFiles/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+         UITextViewWorkaround.unique.executeWorkaround()
         return true
     }
     @available(iOS 13.0, *)
@@ -25,4 +26,45 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 }
+class UITextViewWorkaround: NSObject {
+
+    // --------------------------------------------------------------------
+    // MARK: Singleton
+    // --------------------------------------------------------------------
+    // make it a singleton
+    static let unique = UITextViewWorkaround()
+
+    // --------------------------------------------------------------------
+    // MARK: executeWorkaround()
+    // --------------------------------------------------------------------
+    func executeWorkaround() {
+
+        if #available(iOS 13.2, *) {
+
+            NSLog("UITextViewWorkaround.unique.executeWorkaround(): we are on iOS 13.2+ no need for a workaround")
+
+        } else {
+
+            // name of the missing class stub
+            let className = "_UITextLayoutView"
+
+            // try to get the class
+            var cls = objc_getClass(className)
+
+            // check if class is available
+            if cls == nil {
+
+                // it's not available, so create a replacement and register it
+                cls = objc_allocateClassPair(UIView.self, className, 0)
+                objc_registerClassPair(cls as! AnyClass)
+
+                #if DEBUG
+                NSLog("UITextViewWorkaround.unique.executeWorkaround(): added \(className) dynamically")
+               #endif
+           }
+        }
+    }
+}
+
+
 
