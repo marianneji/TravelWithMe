809c7d3fa32a807461dc83f57137d06216671f56
marianneji <marianneji@gmail.com>
2019-11-14 10:20:14 +0100

    testing issues with translateTests

--- TravelTests/TranslateTests/TranslateTestsCase.swift
+++ TravelTests/TranslateTests/TranslateTestsCase.swift
@@ -65,84 +65,23 @@ class TranslateTestsCase: XCTestCase {
         
 
         let expectation = XCTestExpectation(description: "Wait for queue change.")
-        translateManager.makeRequest(usingTranslateApi: .translate, urlParams: urlParams) { (results) in
+        translateManager.makeRequest(usingTranslateApi: .translate, urlParams: urlParams) { (success, results) in
+//            let translatedText = "Hi cows"
+            XCTAssertNotNil(success)
             XCTAssertNotNil(results)
             expectation.fulfill()
         }
-
         wait(for: [expectation], timeout: 0.01)
     }
-//    func testGetCityshouldPostSuccessCallbackIfDataAndNoError() {
-//        let weatherManager = WeatherManager(weatherSession: URLSessionFake(data: WeatherFakeResponseData.weatherCorrectData, response: WeatherFakeResponseData.responseOk, error: nil))
+//
+//    func testTranslateshouldPostSuccessCallbackIfcorrectData() {
+//        let translateManager = TranslateManager(translateSession: URLSessionFake(data: TranslateFakeResponseData.translateCorrectData, response: TranslateFakeResponseData.responseOk, error: nil))
 //
 //        let expectation = XCTestExpectation(description: "Wait for queue change.")
-//        weatherManager.getCity(city: "Paris") { (success, weather) in
-//            XCTAssertTrue(success)
-//            XCTAssertNotNil(weather)
-//            XCTAssertEqual("Paris", weather?.cityName)
-//
+//        translateManager.translate { (success) in
+//            XCTAssertTrue((success != nil))
 //            expectation.fulfill()
 //        }
 //        wait(for: [expectation], timeout: 0.01)
 //    }
-//    //MARK: - TestGetLocation
-//    func testGetLocationshouldPostfailedCallbackIfError() {
-//        let weatherManager = WeatherManager(weatherSession: URLSessionFake(data: nil, response: nil, error: WeatherFakeResponseData.error))
-//
-//        let expectation = XCTestExpectation(description: "Wait for queue change.")
-//        weatherManager.getCurrentLocationWeather(latitude: 48.86, longitude: 2.35) { (success, weather) in
-//            XCTAssertFalse(success)
-//            XCTAssertNil(weather)
-//            expectation.fulfill()
-//        }
-//        wait(for: [expectation], timeout: 0.01)
-//    }
-//
-//    func testGetLocationshouldPostfailedCallbackWhenNoData() {
-//        let weatherManager = WeatherManager(weatherSession: URLSessionFake(data: nil, response: nil, error: nil))
-//
-//        let expectation = XCTestExpectation(description: "Wait for queue change.")
-//        weatherManager.getCurrentLocationWeather(latitude: 48.86, longitude: 2.35) { (success, weather) in
-//            XCTAssertFalse(success)
-//            XCTAssertNil(weather)
-//            expectation.fulfill()
-//        }
-//        wait(for: [expectation], timeout: 0.01)
-//    }
-//    func testGetLocationshouldPostfailedCallbackIfIncorrectResponse() {
-//        let weatherManager = WeatherManager(weatherSession: URLSessionFake(data: WeatherFakeResponseData.weatherCorrectData, response: WeatherFakeResponseData.responseKo, error: nil))
-//
-//               let expectation = XCTestExpectation(description: "Wait for queue change.")
-//               weatherManager.getCurrentLocationWeather(latitude: 48.86, longitude: 2.35) { (success, weather) in
-//                   XCTAssertFalse(success)
-//                   XCTAssertNil(weather)
-//                   expectation.fulfill()
-//               }
-//               wait(for: [expectation], timeout: 0.01)
-//    }
-//
-//    func testGetLocationshouldPostfailedCallbackIfIncorrectData() {
-//        let weatherManager = WeatherManager(weatherSession: URLSessionFake(data: WeatherFakeResponseData.weatherIncorrectData, response: nil, error: nil))
-//
-//        let expectation = XCTestExpectation(description: "Wait for queue change.")
-//        weatherManager.getCurrentLocationWeather(latitude: 48.86, longitude: 2.35) { (success, weather) in
-//            XCTAssertFalse(success)
-//            XCTAssertNil(weather)
-//            expectation.fulfill()
-//        }
-//        wait(for: [expectation], timeout: 0.01)
-//    }
-//    func testGetLocationshouldPostSuccessCallbackIfDataAndNoError() {
-//        let weatherManager = WeatherManager(weatherSession: URLSessionFake(data: WeatherFakeResponseData.weatherCorrectData, response: WeatherFakeResponseData.responseOk, error: nil))
-//
-//        let expectation = XCTestExpectation(description: "Wait for queue change.")
-//        weatherManager.getCurrentLocationWeather(latitude: 48.86, longitude: 2.35) { (success, weather) in
-//            XCTAssertTrue(success)
-//            XCTAssertNotNil(weather)
-//
-//            expectation.fulfill()
-//        }
-//        wait(for: [expectation], timeout: 0.01)
-//    }
-
 }
